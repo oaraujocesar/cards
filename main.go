f4063ef08ca8703ce0395469dd8d70de1622e7bd
César O. Araújo <oaraujocesar@gmail.com>
2021-10-15 14:38:20 -0300

    [Basics] variables: ways of declaring it

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
 }
