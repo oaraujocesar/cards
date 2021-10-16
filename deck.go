0e6095d0287675224b3a6e4127bc430dbdc08fd7
César O. Araújo <oaraujocesar@gmail.com>
2021-10-16 14:48:48 -0300

    [Basics] Project: Create new deck function

--- deck.go
+++ deck.go
@@ -6,8 +6,23 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit + " of " + value)
+		} 
+	}
+
+	return cards
+}
+
 func (d deck) print() {
-	for index, card := range d {
-		fmt.Println(index,card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
