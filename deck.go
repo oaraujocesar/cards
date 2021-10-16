169ea55795646befe983b33f4ee599bed6168112
César O. Araújo <oaraujocesar@gmail.com>
2021-10-16 15:19:48 -0300

    [Basics] Project: Create the deal function

--- deck.go
+++ deck.go
@@ -26,3 +26,7 @@ func (d deck) print() {
 		fmt.Println(card)
 	}
 }
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 
-	cards.print()
+	hand, remainingDeck := deal(cards, 5)
+
+	hand.print()
+	fmt.Println("")
+	remainingDeck.print()
 }
