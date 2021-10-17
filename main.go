9849eb7bbc9e1eb689b347db4d0c02841a6bd96f
César O. Araújo <oaraujocesar@gmail.com>
2021-10-16 23:13:52 -0300

    [Basics] Project: Save card to the hard drive

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
 
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	fmt.Println("")
-	remainingDeck.print()
+	cards.saveToFile("my_cards")
 }
