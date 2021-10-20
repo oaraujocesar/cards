f3a017a2f2c4759cb9bde7852d7d1f3e07251359
César O. Araújo <oaraujocesar@gmail.com>
2021-10-20 13:08:17 -0300

    [Basics] Testing: Add new assertions to the new deck test

--- deck_test.go
+++ deck_test.go
@@ -8,8 +8,19 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	expectedLength := 52
+	fistCard := d[0]
+	lastCard := d[len(d) - 1]
 
 	if len(d) != expectedLength {
 		t.Errorf("Expected deck length of %v, but got %v", expectedLength, len(d))
 	}
+
+	if fistCard != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", fistCard)
+	}
+
+	if lastCard != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs, but got %v", lastCard)
+	}
 }
+
